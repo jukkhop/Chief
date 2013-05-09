c318a4ebdc5d1112bf2c580e3a699be0693a72ee
Tatu <tatu.ulmanen@virtue.fi>
2013-05-10 02:08:31 +0300

    Database independent of mysql

--- core/database.php
+++ core/database.php
@@ -3,205 +3,215 @@ namespace Chief;
 
 class Database
 {
-    private $connection = false;
+	private $connection = false;
 
-    public function __construct($host = null, $user = null, $pass = null, $db = null)
-    {        
-        if(!is_null($host) && !is_null($user) && !is_null($pass) && !is_null($db)) {
-            return $this->connect($host, $user, $pass, $db);
-        }
-    }
+	public function __construct($host = null, $user = null, $pass = null, $db = null)
+	{		
+		if(!is_null($host) && !is_null($user) && !is_null($pass) && !is_null($db)) {
+			return $this->connect($host, $user, $pass, $db);
+		}
+	}
 
-    # Connect to database
-    public function connect($host, $user, $pass, $db, $driver = 'mysql')
-    {
-        if(empty($user)) return false;
-        try {
-            switch($driver) {
-                case 'mysql':
-                case 'mariadb':
-                    $this->connection = new \PDO(
-                        sprintf('mysql:host=%s;dbname=%s', $host, $db),
-                        $user,
-                        $pass,
-                        [\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]
-                    );
-                    break;
-                case 'pgsql':
-                    $this->connection = new \PDO(
-                        sprintf('pgsql:dbname=%s;host=%s;', $host, $db),
-                        $user,
-                        $pass);
-                    break;
-                case 'sqlsrv':
-                    $this->connection = new \PDO(
-                        sprintf('sqlsrv:Server=%s;Database=%s;', $host, $db),
-                        $user,
-                        $pass);
-                    break;
-                case 'sqlite':
-                    $this->connection = new \PDO(
-                        sprintf('sqlite:%s', $db),
-                        $user,
-                        $pass);
-                    break;
-            }
-            return isset($this->connection) ? $this->connection : false;
-        } catch(Exception $e) {
-            return false;
-        }
-    }
+	# Connect to database
+	public function connect($host, $user, $pass, $db, $driver = 'mysql')
+	{
+		if(empty($user)) return false;
+		try {
+			switch($driver) {
+				case 'mysql':
+				case 'mariadb':
+					$this->connection = new \PDO(
+						sprintf('mysql:host=%s;dbname=%s', $host, $db),
+						$user,
+						$pass,
+						[\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]
+					);
+					break;
+				case 'pgsql':
+					$this->connection = new \PDO(
+						sprintf('pgsql:dbname=%s;host=%s;', $host, $db),
+						$user,
+						$pass);
+					break;
+				case 'sqlsrv':
+					$this->connection = new \PDO(
+						sprintf('sqlsrv:Server=%s;Database=%s;', $host, $db),
+						$user,
+						$pass);
+					break;
+				case 'sqlite':
+					$this->connection = new \PDO(
+						sprintf('sqlite:%s', $db),
+						$user,
+						$pass);
+					break;
+			}
+			return isset($this->connection) ? $this->connection : false;
+		} catch(Exception $e) {
+			return false;
+		}
+	}
 
-    # Internal function for executing queries 
-    private function execute(Array $args)
-    {
-        if(!$this->connection) {
-            throw new Exception('No database connection established.');
-        }
+	# Internal function for executing queries 
+	private function execute(Array $args)
+	{
+		if(!$this->connection) {
+			throw new Exception('No database connection established.');
+		}
 
-        if(empty($args)) {
-            return false;
-        }
+		if(empty($args)) {
+			return false;
+		}
 
-        $sql = array_shift($args);
-        $statement = $this->connection->prepare($sql);
+		$sql = array_shift($args);
+		$statement = $this->connection->prepare($sql);
 
-        try {
-            $statement->execute($args);
-        } catch (\PDOException $e) {
-            echo $e->getMessage()."\n";
-            $trace = $e->getTrace();
-            if(isset($trace[1])) {
-                echo 'Called in '.$trace[1]['file'].' on line '.$trace[1]['line']."\n";
-            }
-        }
-        return $statement;
-    }
+		try {
+			$status = $statement->execute($args);
+			if($status === false) {
+				$error = $statement->errorInfo();
+				echo $error[2];
+				$trace = debug_backtrace();
+				if(isset($trace[1])) {
+					echo 'Called in '.$trace[1]['file'].' on line '.$trace[1]['line']."\n";
+				}
+			}
+		} catch (\PDOException $e) {
+			echo $e->getMessage()."\n";
+			$trace = $e->getTrace();
+			if(isset($trace[1])) {
+				echo 'Called in '.$trace[1]['file'].' on line '.$trace[1]['line']."\n";
+			}
+		}
+		return $statement;
+	}
 
-    # Run a query
-    public function query()
-    {
-        return $this->execute(func_get_args());
-    }
+	# Run a query
+	public function query()
+	{
+		return $this->execute(func_get_args());
+	}
 
-    # Return all rows
-    public function all()
-    {
-        $statement = $this->execute(func_get_args());
-        return $statement->fetchAll(\PDO::FETCH_OBJ);
-    }
+	# Return all rows
+	public function all()
+	{
+		$statement = $this->execute(func_get_args());
+		return $statement->fetchAll(\PDO::FETCH_OBJ);
+	}
 
-    # Return a single column
-    public function column()
-    {
-        $statement = $this->execute(func_get_args());
-        $result = array();
-        foreach($statement->fetchAll() as $row) {
-            $row = (array)$row;
-            $result[] = array_shift($row);
-        }
-        return $result;
-    }
+	# Return a single column
+	public function column()
+	{
+		$statement = $this->execute(func_get_args());
+		$result = array();
+		foreach($statement->fetchAll() as $row) {
+			$row = (array)$row;
+			$result[] = array_shift($row);
+		}
+		return $result;
+	}
 
-    # Return a single row
-    public function row()
-    {
-        $statement = $this->execute(func_get_args());
-        return $statement->fetchObject();
-    }
+	# Return a single row
+	public function row()
+	{
+		$statement = $this->execute(func_get_args());
+		return $statement->fetchObject();
+	}
 
-    # Return the first value in the first row
-    public function one($resource = null)
-    {
-        $statement = $this->execute(func_get_args());
-        return $statement->fetchColumn();
-    }
+	# Return the first value in the first row
+	public function one($resource = null)
+	{
+		$statement = $this->execute(func_get_args());
+		return $statement->fetchColumn();
+	}
 
-    # Return the count of found rows
-    public function count($resource = null)
-    {
-        $statement = $this->execute(func_get_args());
-        return $statement->rowCount();        
-    }
+	# Return the count of found rows
+	public function count($resource = null)
+	{
+		$statement = $this->execute(func_get_args());
+		return $statement->rowCount();		
+	}
 
-    # Escape variables
-    public function escape($str)
-    {
-        return is_scalar($str) ? $this->connection->quote($str) : $str;
-    }
+	# Escape variables
+	public function escape($str)
+	{
+		return is_scalar($str) ? filter_var($str, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW & FILTER_FLAG_STRIP_HIGH) : $str;
+	}
 
-    # Insert
-    public function insert($table, $fields)
-    {
-    	$columns = implode(',', array_keys($fields));
-		$values  = implode(',', array_fill(0, count($fields), '?'));
-		$statement = $this->connection->prepare("INSERT INTO {$table} ({$columns}) VALUES ({$values})");
-		return $statement->execute(array_values($fields)) === false ? false : $this->connection->lastInsertId();
-    }
+	# Insert
+	public function insert($table, $fields)
+	{
+		$query = sprintf("INSERT INTO `%s` SET\n", $this->escape($table));
+		$keys = array();
+		$values = array();
+		foreach($fields as $key => $value) {
+			$keys[] = sprintf("`%s` = ?", $this->escape($key));
+			$values[] = $value;
+		}
+		$query .= implode(",\n", $keys);
+		$statement = $this->execute(array_merge([$query], $values));
+		return $statement === false ? false : $this->connection->lastInsertId();
+	}
 
-    # Replace
-    public function replace($table, $fields)
-    {
-        $query = sprintf("REPLACE INTO `%s` SET\n", $this->escape($table));
-        $values = array();
-        foreach($fields as $key => $value) {
-            if(is_null($value)) {
-                $value = 'NULL';
-            } else {
-                $value = sprintf("'%s'", $this->escape($value));
-            }
-            $values[] = sprintf("`%s` = %s", $this->escape($key), $value);
-        }
-        $query .= implode(",\n", $values);
-        $statement = $this->query($query);
-        return $statement === false ? false : $this->connection->lastInsertId();
-    }
+	# Replace
+	public function replace($table, $fields)
+	{
+		$query = sprintf("REPLACE INTO `%s` SET\n", $this->escape($table));
+		$keys = array();
+		$values = array();
+		foreach($fields as $key => $value) {
+			$keys[] = sprintf("`%s` = ?", $this->escape($key));
+			$values[] = $value;
+		}
+		$query .= implode(",\n", $keys);
+		$statement = $this->execute(array_merge([$query], $values));
+		return $statement === false ? false : $this->connection->lastInsertId();
+	}
 
-    # Update
-    public function update($table, $fields, $where = null)
-    {
-        $query = sprintf("UPDATE %s SET\n", $table);
-        $values = array();
-        foreach($fields as $key => $value) {
-            if(is_null($value)) {
-                $value = 'NULL';
-            } else {
-                $value = $this->escape($value);
-            }
-            $values[] = sprintf("%s = %s", $key, $value);
-        }
-        $query .= implode(",\n", $values)."\n";
+	# Update
+	public function update($table, $fields, $where = null)
+	{
+		$query = sprintf("UPDATE `%s` SET\n", $this->escape($table));
+		$keys = array();
+		$values = array();
+		foreach($fields as $key => $value) {
+			$keys[] = sprintf("`%s` = ?", $this->escape($key));
+			$values[] = $value;
+		}
+		$query .= implode(",\n", $keys);
 
-        if(is_array($where)) {
-            $_where = array();
-            foreach($where as $key => $value) {
-                $_where[] = sprintf("%s = %s", $key, $this->escape($value));
-            }
-            $where = implode(" AND ", $_where);
-        } else {
-            $where = sprintf("`id` = %s", $this->escape($where));
-        }
-        $query .= 'WHERE '.$where;
+		if(is_array($where)) {
+			$_where = array();
+			foreach($where as $key => $value) {
+				$_where[] = sprintf("`%s` = ?", $this->escape($key));
+				$values[] = $value;
+			}
+			$where = implode(" AND ", $_where);
+		} else {
+			$values[] = $where;
+			$where = sprintf("`id` = ?");
+		}
+		$query .= 'WHERE '.$where;
 
-        $statement = $this->query($query);
-        return $statement === false ? false : $statement->rowCount();
-    }
+		$statement = $this->execute(array_merge([$query], $values));
+		return $statement === false ? false : $statement->rowCount();
+	}
 
-    # Begin transaction
-    public function begin()
-    {
-        $this->connection->beginTransaction();
-    }
+	# Begin transaction
+	public function begin()
+	{
+		$this->connection->beginTransaction();
+	}
 
-    # Commit transaction
-    public function commit()
-    {
-        $this->connection->commit();
-    }
+	# Commit transaction
+	public function commit()
+	{
+		$this->connection->commit();
+	}
 
-    # Rollback transaction
-    public function rollback()
-    {
-        $this->connection->rollBack();
-    }
+	# Rollback transaction
+	public function rollback()
+	{
+		$this->connection->rollBack();
+	}
 }
